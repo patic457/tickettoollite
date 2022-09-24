0d9f652ea2d7fbcb48444250a3c7926305bba0c9
sunin457 <sunin457@gmail.com>
2022-09-24 18:48:22 +0700

    Update profile_module.dart

--- module/profile_module/lib/profile_module.dart
+++ module/profile_module/lib/profile_module.dart
@@ -1,27 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   // const Profile({super.key});
-
   const Profile({
     Key? key,
     required this.title,
   }) : super(key: key);
-
   final String title;
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  void _editAddressScreen() {
+    Navigator.pushNamed(context, '/address');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Container(
-        child: Column(children: [
-          Text('Sunisa Mei'),
-        ]),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            Text(
+              'Sunisa Mei',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('address'),
+                TextButton(
+                  onPressed: _editAddressScreen,
+                  child: Icon(
+                    Icons.arrow_forward_ios_sharp,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            )
+          ]),
+        ),
       ),
     );
   }
